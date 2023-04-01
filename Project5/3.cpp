07e9d69987c2a382575af8b7208ec0e03c873b45
Zhengyang Shen <317255300@qq.com>
2023-04-01 10:58:25 +0800

    10:58

--- Project5/3.cpp
+++ Project5/3.cpp
@@ -1,8 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include"stb_image.h"
-#include "shader_s.h"//由于这个头文件已经在工作目录下，所以直接用双引号这样包含就可以
+#include "shader_s.h"
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -11,8 +14,6 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    //int width, height, nrChannels;
-    //unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -31,36 +32,38 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
 
-    //以上为基础操作
-    //这里的两个文件要位于工作目录下，名字可以随便调整，不用动头文件
-    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");//创建自定义类型shader的一个对象名叫ourShader
-
+    //我们要渲染一个矩形，矩形有四个顶点，每个顶点的属性从前到后分为三组，分别是位置，颜色和纹理坐标
     float vertices[] = {
 
-         0.5f,0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f,1.0f,
+         0.5f,0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  1.0f,1.0f,
          0.5f,-0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f,0.0f,
-         -0.5f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f,0.0f,
-         -0.5f,0.5f,0.0f,1.0f,1.0f,0.0f,0.0f,1.0f
+         -0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  0.0f,0.0f,
+         -0.5f,0.5f,0.0f,   1.0f,1.0f,0.0f,    0.0f,1.0f
     };
+    //要指明这几个顶点要如何连接。索引为0,1,3的三个顶点连成一个三角形，索引为1,2,3的连成另一个
     unsigned int indices[] = {
         0,1,3,
         1,2,3
     };
+    //下面是规则属性配置
+    //采用VBO、VAO节省了绘制多个物体时 CPU 与 GPU 之间的通讯时间，提高了渲染性能，缓冲区大多数都是干这个工作的。
     unsigned int VBO, VAO, EBO;
+    
     glGenVertexArrays(1, &VAO);
+    //下面创建缓冲对象
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-
+    //首先，绑定VAO，以告知OpenGL程序该使用这个VAO来对VBO做出解释。
     glBindVertexArray(VAO);
-
+    //绑定第一个VBO，向这个VBO中写入数据，告知VAO该如何解释这个VBO的信息；然后，解绑这个VBO。glBufferData用来给缓冲对象分配空间以及存储数据，这样我们在vertices里面的数据就可以存入VBO了
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // position attribute
+    //glVertexAttribPointer的用途是将这个属性存入VAO并激活（enable激活），同时每个属性的索引也会留下（也就是下文的0,1,2）
+    //直到VAO，电脑（显卡GPU）才知道你刚才写在vertices里面的数据分别对应什么属性，几个为一组
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
@@ -69,18 +72,20 @@ int main()
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
-    //以上为基础操作
     unsigned int texture;
     glGenTextures(1, &texture);
+    //绑到GL_TEXTURE_2D上去
     glBindTexture(GL_TEXTURE_2D, texture);
+    //告诉显卡如何画（纹理过滤方法）
     glad_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glad_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glad_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glad_glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    int width, height, nrChannels;
+    int width, height, nrChannels;//颜色通道的个数
     unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
+        //下面一行的函数用来生成一张纹理。第一个参数指定了纹理目标(Target)。设置为GL_TEXTURE_2D意味着会生成与当前绑定的纹理对象在同一个目标上的纹理（任何绑定到GL_TEXTURE_1D和GL_TEXTURE_3D的纹理不会受到影响）。
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
